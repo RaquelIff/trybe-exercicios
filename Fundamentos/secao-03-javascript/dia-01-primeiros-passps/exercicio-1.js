9a735b7bfb103589dce2c8841e8bc0e0abcc503d
Raquel <raqueliff@uol.com.br>
2023-01-25 23:10:49 +0000

    exercício iniciado

---
+++ Fundamentos/secao-03-javascript/dia-01-primeiros-passps/exercicio-1.js
@@ -0,0 +1,20 @@
+const valor1 = 8;
+const valor2 = 3;
+
+if (valor1 > valor2) {
+    console.log(valor1)
+} else {
+    console.log(valor2)
+}
+
+const valor3 = 12;
+const valor4 = 18;
+const valor5 = 206;
+
+if (valor3 > valor4 && valor3 > valor5) {
+    console.log(valor3)
+} else if (valor4 > valor3 && valor4 > valor5) {
+    console.log(valor4)
+} else if (valor5 > valor3 && valor5 > valor4) {
+    console.log(valor5)
+    }
