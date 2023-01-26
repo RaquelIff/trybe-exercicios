5b083c4797322dd9105b37734e3891d227cee8b8
Raquel <raqueliff@uol.com.br>
2023-01-26 22:49:27 +0000

    exercício iniciados

---
+++ Fundamentos/secao-03-javascript/dia-4-objetos-e-funcoes/exercicio-1.js
@@ -0,0 +1,14 @@
+let player = {
+   pessoa :'Marta',
+    lastPessoa : 'Silva',
+    age : 34,
+    medals : { golden: 2, silver: 3 },
+}
+
+console.log('A jogadora ' + player.pessoa + ' ' + player.lastPessoa + ' tem ' + player.age + ' de idade.');
+
+player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
+
+console.table(player);
+
+console.log('A jogadora ' + player['pessoa'] + ' ' + player['lastPessoa'] + ' foi eleita a melhor do mundo por ' + player['bestInTheWorld'].length + ' vezes.');
